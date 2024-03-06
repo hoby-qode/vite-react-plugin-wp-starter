441e1bfa7a09b40d257aefbc2e0785c0b4395e79
uix-dev <houbby22@gmail.com>
2024-03-06 07:25:17 -0800

    feat: mise en forme de la page commande

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react'],
-          'react-dom': ['react-dom']
-        }
-      }
-    },
-  },
 })
